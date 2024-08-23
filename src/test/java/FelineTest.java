d1a8e2d570bc3d6cedd8f61d6603d82c55dc8439
Igor Ponomarev <Alfrankov19950606@yandex.com>
2024-08-23 09:12:53 +0300

    Юнит тесты

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,2 +1,61 @@
-package PACKAGE_NAME;public class FelineTest {
+import com.example.Feline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doReturn;
+
+@RunWith(Parameterized.class)
+public class FelineTest {
+
+    private final List<String> expectedFood;
+    private final int inputKittens;
+    private final int expectedKittens;
+    private final String expectedFamily;
+
+    public FelineTest(List<String> expectedFood, int inputKittens, int expectedKittens, String expectedFamily) {
+        this.expectedFood = expectedFood;
+        this.inputKittens = inputKittens;
+        this.expectedKittens = expectedKittens;
+        this.expectedFamily = expectedFamily;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {FelineTestData.getEatMeatTestData().get(0)[0], 1, 1, FelineTestData.getFamilyTestData().get(0)[0]},
+                {null, 3, 3, FelineTestData.getFamilyTestData().get(0)[0]}
+        });
+    }
+
+    @Test
+    public void testEatMeat() throws Exception {
+        if (expectedFood != null) {
+            Feline feline = Mockito.spy(new Feline());
+            doReturn(expectedFood).when(feline).getFood("Хищник");
+            List<String> result = feline.eatMeat();
+            assertEquals(expectedFood, result);
+        }
+    }
+
+    @Test
+    public void testGetKittens() {
+        Feline feline = new Feline();
+        int result = (inputKittens == 1) ? feline.getKittens() : feline.getKittens(inputKittens);
+        assertEquals(expectedKittens, result);
+    }
+
+    @Test
+    public void testGetFamily() {
+        Feline feline = new Feline();
+        String result = feline.getFamily();
+        assertEquals(expectedFamily, result);
+    }
 }
--- src/test/java/FelineTestData.java
+++ src/test/java/FelineTestData.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class FelineTestData {
+import java.util.Arrays;
+import java.util.List;
+
+public class FelineTestData {
+
+    public static List<Object[]> getEatMeatTestData() {
+        return Arrays.asList(new Object[][] {
+                {Arrays.asList("Животные", "Птицы", "Рыба")}
+        });
+    }
+
+    public static List<Object[]> getKittensTestData() {
+        return Arrays.asList(new Object[][] {
+                {1, 1},
+                {3, 3}
+        });
+    }
+
+    public static List<Object[]> getFamilyTestData() {
+        return Arrays.asList(new Object[][] {
+                {"Кошачьи"}
+        });
+    }
 }
